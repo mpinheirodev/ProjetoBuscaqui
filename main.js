5e441c415c2da3109c30f93d04ba74ddb013e8ed
MedusaGabriel <gabrieloliveirabarros1997@gmail.com>
2023-11-18 20:29:30 -0300

    modal login

--- main.js
+++ main.js
@@ -58,3 +58,18 @@ document.addEventListener('mousedown', (e) => {
     DropdownContent.classList.remove("mostrar");
   }
 })
+
+// Modal
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+  modal.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
