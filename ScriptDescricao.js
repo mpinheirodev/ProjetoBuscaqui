eae79de0966785171346f3ebcd74d1bdf96b5e39
EvandroFrança <evandrofilho20111@gmail.com>
2023-11-18 05:12:20 -0300

    Atualização tela de descrição
    Feito card de variação de preço, card de notificação. Adicionado um pequeno texto para o termo de uso.
    Quebrei a cabeça mas fiz, terminando 5 da manhâ, bem vampirinho.

--- ScriptDescricao.js
+++ ScriptDescricao.js
@@ -1,30 +1,35 @@
-    //Script para carrosel de fotos
 document.addEventListener("DOMContentLoaded", function () {
-    const mainImage = document.querySelector(".imagem_principal");
-    const thumbnails = document.querySelectorAll(".thumbnail");
-  
-    thumbnails.forEach((thumbnail) => {
-      thumbnail.addEventListener("click", () => {
-        mainImage.src = thumbnail.src;
-        mainImage.alt = thumbnail.alt;
-      });
+  const mainImage = document.querySelector(".imagem_principal");
+  const thumbnails = document.querySelectorAll(".thumbnail");
+
+  thumbnails.forEach((thumbnail) => {
+    thumbnail.addEventListener("click", () => {
+      mainImage.src = thumbnail.src;
+      mainImage.alt = thumbnail.alt;
     });
   });
-  
-  function showTab(tabId) {
+
+  // Adicione este trecho para garantir que a primeira aba esteja ativa por padrão
+  const defaultTab = document.getElementById("precos");
+  defaultTab.classList.add("active");
+});
+
+// ... Seu código JavaScript anterior ...
+
+function showTab(tabId) {
     // Oculta todos os conteúdos das abas
     document.querySelectorAll('.tab-content').forEach(tab => {
-      tab.classList.remove('active');
+        tab.classList.remove('active');
     });
-  
+
     // Remove a classe 'active' de todos os botões
     document.querySelectorAll('.tab-button').forEach(button => {
-      button.classList.remove('active');
+        button.classList.remove('active');
     });
-  
+
     // Exibe o conteúdo da aba clicada
     document.getElementById(tabId).classList.add('active');
-  
+
     // Adiciona a classe 'active' ao botão clicado
     event.target.classList.add('active');
-  }
+}
