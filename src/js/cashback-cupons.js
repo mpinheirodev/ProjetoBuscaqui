ae795a0c239e0804417292cf9dd87b48a5a584dc
Anderson Djalma <122568258+andersondjalma@users.noreply.github.com>
2023-11-23 04:43:10 -0300

    Atualizações detalhes

--- src/js/cashback-cupons.js
+++ src/js/cashback-cupons.js
@@ -1,46 +1,8 @@
-
 // scroll navbar
 
 window.addEventListener("scroll", () => {
     document.querySelector("nav").classList.toggle("window-scroll", window.scrollY > 0)
 })
-
-
-// Carrossel
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// controles Proxima/anterior 
-function passarSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-// Thumbnail imagem controles
-function slideAtual(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slide");
-  let botoes = document.getElementsByClassName("botao");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < botoes.length; i++) {
-    botoes[i].className = botoes[i].className.replace(" ativo", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  botoes[slideIndex - 1].className += " ativo";
-}
-
 
 // Dropdown de categorias
 
